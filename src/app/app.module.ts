e60fbc52a5363d6dd98e0f71a52d0f145f080de2
Eric VandeGriek <evandegr@akamai.com>
2017-12-18 18:19:05 -0500

    updates and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/site/home/home.component';
+import { GameScreenComponent } from './components/site/game-screen/game-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameScreenComponent
   ],
   imports: [
     BrowserModule
